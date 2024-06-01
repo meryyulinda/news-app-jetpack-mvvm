e783ac195a54ded7cfd9392c64faefc98fe11a61
Mery Yulinda <meryrahmi@gmail.com>
2024-06-01 13:16:46 +0700

    onboarding viewmodel & upgrade gradle plugin ver. + compileSdk

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -28,7 +28,9 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreen() {
+fun OnBoardingScreen(
+    event: (OnBoardingEvent) -> Unit
+) {
     Column(modifier = Modifier.fillMaxSize()) {
         val pagerState = rememberPagerState(initialPage = 0) {
             pages.size
@@ -83,8 +85,8 @@ fun OnBoardingScreen() {
                     text = buttonState.value[1],
                     onClick = {
                         scope.launch {
-                            if (pagerState.currentPage == 3) {
-                                //TODO: Navigate to Home screen
+                            if (pagerState.currentPage == 2) {
+                                event(OnBoardingEvent.SaveAppEntry)
 
                             } else {
                                 pagerState.animateScrollToPage(
